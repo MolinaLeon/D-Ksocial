a2be4062e726c3b196d7fbf6c478163dab31ea7e
MolinaLeon <31654307+MolinaLeon@users.noreply.github.com>
2017-11-28 21:37:29 +0100

    createUser
    CreateUser "virker", men rammer ikke databasen

--- js/createStudent.js
+++ js/createStudent.js
@@ -6,25 +6,25 @@ $(document).ready(() => {
         const lastName = $("#regLastName").val();
         const email = $("#regMail").val();
         const password = $("#regPassword").val();
-        const verifyPassword = $("#regVerifyPassword");
+        const verifyPassword = $("#regVerifyPassword").val();
 
         if (!firstName || !lastName || !email || !password || !verifyPassword) {
             alert("Alle felter skal udfyldes")
         } else {
-            if (password !== verifyPassword) {
-                alert("De indtastede koder stemmer ikke overens");
-                return;
-            }
-
+            if (password.valueOf() === verifyPassword.valueOf()) {
                 SDK.Student.register(regFirstName, regLastName, regMail, regPassword, regVerifyPassword, (err, data) => {
                     if (err && err.xhr.status === 400) {
                         $(".form-group").addClass("has-error");
                     } else {
+                        console.log(SDK.Student.register(regFirstName));
                         alert("Du er nu oprettet som: " + firstName + "\nDin mail er dit brugernavn");
                         window.location.href = "login.html";
                     }
                 });
+            } else {
+                alert("De indtastede koder stemmer ikke overens")
             }
+        }
     });
 $ ("#back-button").click(() => {
     window.location.href = "index.html"
