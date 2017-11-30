1e9ad7e191ac6983e6e656524e41314fcfd284ee
MolinaLeon <31654307+MolinaLeon@users.noreply.github.com>
2017-11-30 10:02:44 +0100

    CreateStudent virker

--- js/createStudent.js
+++ js/createStudent.js
@@ -2,19 +2,22 @@ $(document).ready(() => {
 
     $("#createUser-button").click(() => {
 
-        const firstName = $("#regFirstName").val();
-        const lastName = $("#regLastName").val();
-        const email = $("#regMail").val();
-        const password = $("#regPassword").val();
-        const verifyPassword = $("#regVerifyPassword").val();
+        let firstName = $("#regFirstName").val();
+        let lastName = $("#regLastName").val();
+        let email = $("#regMail").val();
+        let password = $("#regPassword").val();
+        let verifyPassword = $("#regVerifyPassword").val();
 
         if (!firstName || !lastName || !email || !password || !verifyPassword) {
             alert("Alle felter skal udfyldes")
         } else {
             if (password.valueOf() === verifyPassword.valueOf()) {
-                SDK.Student.register(regFirstName, regLastName, regMail, regPassword, regVerifyPassword, (err, data) => {
+                SDK.Student.register(firstName, lastName, email, password, verifyPassword, (err, data) => {
                     if (err && err.xhr.status === 400) {
                         $(".form-group").addClass("has-error");
+
+                    }else if (err){
+                        console.log(err);
                     } else {
                         alert("Du er nu oprettet som: " + firstName + "\nDin mail, " + email + ", er dit brugernavn");
                         window.location.href = "login.html";
